15426e37f51ed6a91bd1ece74cf3e0cc710873d0
Nichlas Handstedt <nichlas.handstedt@scania.com>
2019-02-13 17:41:19 +0100

    test

--- main.tf
+++ main.tf
@@ -6,12 +6,8 @@ provider "aws" {
   region = "${var.region}"
 }
 
-provider "gitlab" {
-
-}
-
 module "kops-cluster-role" {
-  source = "git::https://gitlab-ci-token:${GITLAB_TOKEN}@gitlab.com/digitaldealer/infra/terraform-modules.git//modules/kops-cluster-role-module"
+  source = "git::https://gitlab.com/digitaldealer/infra/terraform-modules.git?ref=v0.0.8//modules/kops-cluster-role-module"
   assuming-account-id = "744147648467"
 }
 
